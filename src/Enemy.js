6748500c375d16a008d3b0057dc4161148914c15
Zaichic <zaipowell@rocketmail.com>
2022-10-02 02:13:57 -0700

    removed unnecessary logging

--- src/Enemy.js
+++ src/Enemy.js
@@ -11,7 +11,6 @@ class Enemy {
         this.context.walls.forEach((wall) => {
             this.context.physics.add.collider(wall.sprite, this.spr_enemy, (wall) => {
                 if (!wall.parent_object.hit) {
-                    console.log(wall);
                     wall.parent_object.hit = true;
                     wall.parent_object.health--;
                     wall.parent_object.resetHit();
